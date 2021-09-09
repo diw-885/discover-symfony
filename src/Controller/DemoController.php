efaf9bb772b4fc1e21e32ddd132bec4176991e03
Matthieu Mota <matthieumota@gmail.com>
2021-09-09 17:18:07 +0200

    Request object and array

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,6 +26,11 @@ class DemoController extends AbstractController
     {
         return $this->render('demo/list.html.twig', [
             'page' => $page,
+            'cars' => [
+                ['brand' => 'Renault'],
+                ['brand' => 'Peugeot'],
+                ['brand' => 'Citroen'],
+            ],
         ]);
     }
 
@@ -37,6 +43,36 @@ class DemoController extends AbstractController
     {
         return $this->render('demo/show.html.twig', [
             'title' => $title,
+        ]);
+    }
+
+    /**
+     * Un exemple de formulaire sans le composant Form.
+     *
+     * @Route("/discover/create", name="demo_create")
+     */
+    public function create(Request $request)
+    {
+        // Avec Symfony, on n'utilise plus $_GET ou $_POST
+        // On va utiliser un objet Request
+        dump($request);
+        // Equivalent de $_GET['page'] ?? null
+        dump($request->query->get('page'));
+        dump($request->get('page')); // Raccourci
+
+        // Equivalent de $_POST['message'] ?? null
+        dump($request->request->get('message'));
+        dump($request->get('message'));
+
+        // On peut faire des redirections et/ou créer des messages flashs
+        if ($request->get('message') === 'secret') {
+            $this->addFlash('success', 'Le message a été envoyé');
+            // return $this->redirect('https://google.fr');
+            return $this->redirectToRoute('demo_list');
+        }
+
+        return $this->render('demo/create.html.twig', [
+            'message' => $request->get('message'),
         ]);
     }
 }
